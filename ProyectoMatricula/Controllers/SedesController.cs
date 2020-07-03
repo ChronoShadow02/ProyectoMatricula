6f683209a6a9c2abaed010ca053f92208d53f9b6
Angel Vega <andresvegacordero007@gmail.com>
2020-07-03 00:43:44 -0600

    Sedes

---
+++ ProyectoMatricula/Controllers/SedesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoMatricula.Modelos;
+namespace ProyectoMatricula.Controllers
+{
+    public class SedesController : Controller
+    {
+
+
+        // GET: Sedes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Muestra la lista de las sedes
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SedesLista()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// Ingresa los datos de las sedes
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SedesNuevo()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Modifica los datos de las sedes
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SedesModifica()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoMatricula/Scripts/Sedes/validarSedes.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+    validarSedes();
+});
+
+function validarSedes() {
+    $("#frmSedes").validate({
+        rules: {
+            Nombre_Sede: {
+                required: true,
+                maxlength: 50
+            },
+            Codigo_Sede: {
+                required: true,
+                maxlength: 50
+            },
+            Id_Director: {
+                required:true
+            },
+            Id_Provincia: {
+                required: true
+            },
+            Id_Canton: {
+                required: true
+            },
+            Id_Distrito: {
+                required: true
+            },
+            Direccion_Fisica: {
+                required: true,
+                maxlength:50
+            }
+        }
+    });
+}
