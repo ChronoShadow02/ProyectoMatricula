bd90200607485f46f2ca7fad276d1983d0fe8de7
Angel Vega <andresvegacordero007@gmail.com>
2020-07-18 13:18:18 -0600

    Actualizacion de fechas de estudiantes y funcionarios

---
+++ ProyectoMatricula/Scripts/Funcionario/JQueryUIFuncionario.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    crearDatepickerFecha();
+});
+
+/*Crea el formato datepicker al input*/
+function crearDatepickerFecha() {
+    $("#txtFechaContratacion").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        ///Se muestran 50 años antes hasta el 2021
+        ///ese "c" es el año actual
+        yearRange: "c-50:c+1",
+        dateFormat: "yy/mm/dd"
+    });
+}
